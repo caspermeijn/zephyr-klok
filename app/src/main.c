c4ca34b8419a2d184074d0bb317d103fd9033cc2
Casper Meijn <casper@meijn.net>
2021-07-11 20:34:26 +0200

    Remove extra files from template

--- app/src/main.c
+++ app/src/main.c
@@ -7,7 +7,6 @@
 #include <drivers/sensor.h>
 
 #include "app_version.h"
-#include "foo.h"
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
@@ -18,37 +17,5 @@ void main(void)
 	const struct device *sensor;
 
 	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-
-	ret = app_foo_bar();
-	if (ret < 0) {
-		LOG_ERR("app_foo_bar failed (%d)", ret);
-		return;
-	}
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return;
-	}
-
-	while (1) {
-		struct sensor_value val;
-
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
-	}
 }
 
